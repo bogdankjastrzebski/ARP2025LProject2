9eff825f08671376b136517fec71c37fd1528e35
bogdankjastrzebski <bogdan.k.jastrzebski@gmail.com>
2025-04-22 16:31:37 +0200

    knitr + data

--- test/test_package.R
+++ test/test_package.R
@@ -1,7 +1,9 @@
 
 # roxygen2::roxygenize()
+# devtools::install_github(repo = "bogdankjastrzebski/ARP2025LProject2")
 devtools::install("/home/bodo/Documents/GitHub/ARP2025LProject2")
 library(ARP2025LProject2)
+
 
 test = function() {
 
@@ -11,15 +13,36 @@ test = function() {
     Y = linreg_data$Y
     B = linreg_data$B
 
-    linreg = linear_regression(X, Y, reg=0.1)
+    k = 100
+    train_ind = 1:k
+    valid_ind = (k+1):nrow(X)
+    linreg = linear_regression(X[train_ind,], Y[train_ind], reg=0.1)
 
     plot(linreg)
+
+    evaluate(linreg, X[valid_ind,], Y[valid_ind])
+
 }
 
 
 make_example_data = function() {
 
     example_data = generate_data(128, 16)
-    save(example_data, file="../data/example_data.RData")
+    save(example_data, file="data/example_data.RData")
+
+}
+
+
+knit_tutorial = function() {
+
+    # install.packages("rmarkdown")
+    # install.packages("knitr")
+
+    library(knitr)
+    library(rmarkdown)
+    
+    knit("Tutorial.Rmd")
+
+    rmarkdown::render("Tutorial.Rmd")
 
 }
